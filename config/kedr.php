55266ed470506e22f693bf301fb6005937104105
vimbatu <vimbatu@yandex.ru>
2025-11-11 15:29:34 +0300

    данные подключения из конфига перенесены в env

--- config/kedr.php
+++ config/kedr.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'url' => 'https://kedr.cloud/api/web/',
-    'api_key' => 'fbf6f5cc-1955-4242-bd6c-f99ddebaa263',
+    'url' => env('KEDR_API_URL'),
+    'api_key' => env('KEDR_API_TOKEN'),
 ];
